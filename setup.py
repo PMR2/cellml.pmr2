1860148b0aa89c41c96bdd5d0541b4f6025b4248
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-01-29 00:50:01 +0000

    cellml.pmr2 template as generated by paster.
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/cellml.pmr2/trunk@4543 eb92df5b-a40c-0410-b02c-bf52feaaa166

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = 'trunk'
+
+setup(name='cellml.pmr2',
+      version=version,
+      description="CellML view plugins for PMR2.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Tommy Yu',
+      author_email='tommy.yu@auckland.ac.nz',
+      url='http://www.cellml.org/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cellml'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
