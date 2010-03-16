ce3e1cc98b37f3df784e01701dcadc90cf8ec426
tyu030 <tyu030@eb92df5b-a40c-0410-b02c-bf52feaaa166>
2010-03-16 07:12:54 +0000

    Added an extractor of license information from CellML files, using the metadata library.
    Progress towards:
    https://tracker.physiomeproject.org/show_bug.cgi?id=2413
    
    git-svn-id: https://svn.physiomeproject.org/svn/physiome/cellml.pmr2/trunk@4777 eb92df5b-a40c-0410-b02c-bf52feaaa166

---
+++ cellml/pmr2/citation.py
@@ -0,0 +1,22 @@
+from cStringIO import StringIO
+import zope.component
+
+from pmr2.app.interfaces import IExposureSourceAdapter
+from pmr2.processor.cmeta import Cmeta
+from pmr2.annotation.citation.utility import CitationFormatterBase
+
+
+class CellMLCitationFormatter(CitationFormatterBase):
+    """\
+    Extracts dcterms:license from CellML file.
+    """
+
+    title = u'CellML RDF Metadata'
+
+    def extract(self):
+        helper = zope.component.queryAdapter(
+            self.context, IExposureSourceAdapter)
+        input = helper.file()
+        metadata = Cmeta(StringIO(input))
+        license = metadata.get_license()
+        return license
