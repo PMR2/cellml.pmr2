cce18d44b0393c90d4ac267e365e5668d48600bd
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-05 19:46:48 +1200

    Well then, a new strategy is needed for live tests
    - Have to make use of the newer/better plone.app.testing layers to setup
      our test data.
    - Wrote the test for testing the full roundtrip for the code generation
      using spawned test ZServer.

---
+++ cellml/pmr2/tests/layer.py
@@ -0,0 +1,90 @@
+import unittest
+from os.path import dirname, join
+
+import zope.component
+from Products.CMFCore.utils import getToolByName
+import pmr2.app
+
+from plone.testing.z2 import ZSERVER
+from plone.testing.z2 import ZSERVER_FIXTURE
+from plone.app.testing.layers import PLONE_ZSERVER
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+
+from plone.app.testing.interfaces import TEST_USER_ID
+from plone.app.testing import helpers
+
+from pmr2.app.exposure.tests.layer import ExposureLayer
+
+from pmr2.testing.base import TestRequest
+
+
+class CellMLExposureLayer(ExposureLayer):
+
+    def setUpZope(self, app, configurationContext):
+        super(CellMLExposureLayer, self).setUpZope(app, configurationContext)
+
+        import cellml.pmr2
+        import cellml.api.pmr2
+        self.loadZCML(package=cellml.api.pmr2)
+        self.loadZCML(package=cellml.pmr2)
+        self.loadZCML('test.zcml', package=cellml.pmr2.tests)
+
+    def setUpPloneSite(self, portal):
+        super(CellMLExposureLayer, self).setUpPloneSite(portal)
+        self.applyProfile(portal, 'cellml.pmr2:default')
+
+        from pmr2.app.workspace.interfaces import IStorageUtility
+        from pmr2.app.workspace.content import Workspace
+        from pmr2.app.exposure.browser.browser import ExposureAddForm
+        from pmr2.app.exposure.browser.browser import ExposureFileGenForm
+
+        import pmr2.testing
+
+        # load the rdfmodel test data
+        target = join(dirname(pmr2.testing.__file__), 'data', 'rdfmodel')
+        su = zope.component.getUtility(IStorageUtility, name='dummy_storage')
+        su._loadDir('rdfmodel', target)
+
+        # add workspace object
+        w = Workspace('rdfmodel')
+        w.storage = 'dummy_storage'
+        portal.workspace['rdfmodel'] = w
+
+        # publish this workspace
+        helpers.setRoles(portal, TEST_USER_ID, ['Manager'])
+        pw = getToolByName(portal, "portal_workflow")
+        pw.doActionFor(portal.workspace['rdfmodel'], 'publish')
+        helpers.setRoles(portal, TEST_USER_ID, ['Member', 'Authenticated',])
+
+        # poke in the exposure
+        request = TestRequest(
+            form={
+                'form.widgets.workspace': u'rdfmodel',
+                'form.widgets.commit_id': u'2',
+                'form.buttons.add': 1,
+            })
+        testform = ExposureAddForm(portal.exposure, request)
+        testform.update()
+        exp_id = testform._data['id']
+        context = portal.exposure[exp_id]
+        self.exposure1 = context
+        rdfmodel = portal.workspace.rdfmodel
+        self.file1 = u'example_model.cellml'
+        request = TestRequest(
+            form={
+                'form.widgets.filename': [self.file1],
+                'form.buttons.add': 1,
+            })
+        testform = ExposureFileGenForm(context, request)
+        testform.update()
+        # store this path for easy access by test case
+        self['exposure_file1_path'] = context[self.file1].getPhysicalPath()
+
+CELLML_EXPOSURE_FIXTURE = CellMLExposureLayer()
+
+# reduce the bases to the minimum set and fix the import at the top
+CELLML_EXPOSURE_INTEGRATION_LAYER = IntegrationTesting(
+    bases=(CELLML_EXPOSURE_FIXTURE, PLONE_ZSERVER),
+    name="cellml.pmr2:exposure_all_integration",
+)
---
+++ cellml/pmr2/tests/test_codegen.py
@@ -0,0 +1,44 @@
+from os.path import dirname, join
+import unittest
+
+import zope.component
+from plone.testing import z2
+from Products.PloneTestCase import PloneTestCase as ptc
+
+from pmr2.app.exposure.browser.browser import ExposureFileAnnotatorForm
+
+from pmr2.testing.base import TestRequest
+
+from cellml.pmr2.tests.layer import CELLML_EXPOSURE_INTEGRATION_LAYER
+
+
+class CodegenTestCase(unittest.TestCase):
+
+    layer = CELLML_EXPOSURE_INTEGRATION_LAYER
+
+    def test_base_codegen(self):
+        context = self.layer['portal'].unrestrictedTraverse(
+            self.layer['exposure_file1_path'])
+        request = TestRequest(
+            form={
+                'form.widgets.annotators': [u'cellml_codegen'],
+                'form.buttons.apply': 1,
+            })
+        view = ExposureFileAnnotatorForm(context, request)
+        view.update()
+        note = zope.component.getAdapter(context, name='cellml_codegen')
+        langkeys = note.code.keys()
+        self.assertIn('C', langkeys)
+        self.assertIn('F77', langkeys)
+        self.assertIn('MATLAB', langkeys)
+        self.assertIn('Python', langkeys)
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(CodegenTestCase))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
+
