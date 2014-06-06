6c88bf1726517b89a1e19bdf04fbee10ded25997
Tommy Yu <tommy.yu@auckland.ac.nz>
2014-06-06 19:05:11 +1200

    Test case for embedded workspace
    - This one will be hugely trickly, as we need a way to resolve mappings
      between all possible vhosts configured for this particular instance of
      PMR.  Will figure out the code to do this at some point.

--- cellml/pmr2/tests/test_urlopener.py
+++ cellml/pmr2/tests/test_urlopener.py
@@ -15,6 +15,18 @@ class UrlOpenerLocalTestCase(unittest.TestCase):
 
     layer = CELLML_EXPOSURE_INTEGRATION_LAYER
 
+    # test standard way, ala the default codegen test.
+    # test various failure modes
+    # - security
+    # - wrong object type
+    # - wrong encoding of uri
+    # - missing file
+    # - missing object
+    # - missing revision
+    # - invalid protocol (ftp:..)
+    # fallback modes, i.e. fallback to http://
+    # relative uri access to embedded workspaces
+
     def test_safe_standard_load_standard(self):
         f = opener.loadURL('pmr:/plone/workspace/test:2:dir1/f2')
         self.assertEqual(f, 'second file in dir1\n')
@@ -28,6 +40,11 @@ class UrlOpenerLocalTestCase(unittest.TestCase):
         logout()
         f = opener.loadURL('pmr:/plone/workspace/main_bucket:0:README')
         self.assertEqual(f, 'A test main repo.\n')
+
+    def test_embedded_load(self):
+        f = opener.loadURL(
+            'pmr:/plone/workspace/external_root:0:external_test/test.txt')
+        self.assertEqual(f, 'external test file.\n')
 
 
 class UrlOpenerSpawnedTestCase(unittest.TestCase):
