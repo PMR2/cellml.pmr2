541b1e2cfccf90c994fb2e9aef636184a4e80e1c
Tommy Yu <tommy.yu@auckland.ac.nz>
2015-08-04 19:35:18 +1200

    Make compatible with collection+json
    - Make use of the core helper methods in the package, based on existing
      code.
    - However the value for certain fields in the the cmeta view is NOT a
      string as specified in the Collection+JSON spec.  Will have to revisit
      this.

--- cellml/pmr2/browser/json.py
+++ cellml/pmr2/browser/json.py
@@ -1,6 +1,30 @@
 from cellml.pmr2.browser.view import CmetaNote, CellMLCodegenNote
+
 from pmr2.json.mixin import JsonPage
 
+from pmr2.json.collection.core import keyvalue_to_itemdata
+from pmr2.json.collection.mixin import JsonCollectionPage
+
+
+def cmeta_note_dict(cmeta_note):
+    return {
+        'title': cmeta_note.model_title,
+        'model_author': cmeta_note.model_author,
+        'model_author_org': cmeta_note.model_author_org,
+        'citation_authors': cmeta_note.citation_authors,
+        'citation_title': cmeta_note.citation_title,
+        # a mismatch, but I blame the metadata spec.
+        'citation_journal': cmeta_note.citation_bibliographicCitation,
+        'citation_id': cmeta_note.citation_id,
+        'keywords': cmeta_note.keywords,
+    }
+
+def cellml_codegen_links(form):
+    results = {}
+    for lang in form.available_langs():
+        results[lang] = ('%s/%s/%s' %
+            (form.context.absolute_url(), form.__name__, lang))
+    return results
 
 class JsonCmetaNote(JsonPage, CmetaNote):
     """
@@ -8,17 +32,7 @@ class JsonCmetaNote(JsonPage, CmetaNote):
     """
 
     def render(self):
-        return self.dumps({
-            'title': self.note.model_title,
-            'model_author': self.note.model_author,
-            'model_author_org': self.note.model_author_org,
-            'citation_authors': self.note.citation_authors,
-            'citation_title': self.note.citation_title,
-            # a mismatch, but I blame the metadata spec.
-            'citation_journal': self.note.citation_bibliographicCitation,
-            'citation_id': self.note.citation_id,
-            'keywords': self.note.keywords,
-        })
+        return self.dumps(cmeta_note_dict(self.note))
 
 
 class JsonCellMLCodegenNote(JsonPage, CellMLCodegenNote):
@@ -30,9 +44,30 @@ class JsonCellMLCodegenNote(JsonPage, CellMLCodegenNote):
         if self.language:
             return self.dumps({self.language: self.content()})
 
-        results = {}
-        for lang in self.available_langs():
-            results[lang] = ('%s/%s/%s' %
-                (self.context.absolute_url(), self.__name__, lang))
-
+        results = cellml_codegen_links(self)
         return self.dumps(results)
+
+
+class JsonCollectionCmetaNote(JsonCollectionPage, CmetaNote):
+    """
+    Cmeta note.
+    """
+
+    def update(self):
+        super(JsonCollectionCmetaNote, self).update()
+        self._jc_items = [keyvalue_to_itemdata(cmeta_note_dict((self.note)))]
+
+
+class JsonCollectionCellMLCodegenNote(JsonCollectionPage, CellMLCodegenNote):
+    """
+    Code generation.
+    """
+
+    def update(self):
+        super(JsonCollectionCellMLCodegenNote, self).update()
+        if self.language:
+            self._jc_items = [keyvalue_to_itemdata(
+                {self.language: self.content()}
+            )]
+        else:
+            self._jc_links = cellml_codegen_links(self)
